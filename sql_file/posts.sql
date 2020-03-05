5fd9fd0c180cc7c109b49f38ec5d8d05e90cf17d
Hussien Abdulkadir Ahmed <abdulkadir.hussien@he-arc.ch>
2020-03-05 14:33:37 +0100

    initial commit based on the existing framework

--- sql_file/posts.sql
+++ sql_file/posts.sql
@@ -8,10 +8,3 @@
 -- Table structure for table `posts`
 --
 
-CREATE TABLE `posts` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `body` text NOT NULL,
-  `created_at` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
