7a12924a1be4759ed8c902c52bf0c469e13bd989
Hussien Abdulkadir Ahmed <abdulkadir.hussien@he-arc.ch>
2020-04-06 02:08:19 +0200

    added update functionality

--- app/routes.php
+++ app/routes.php
@@ -8,19 +8,11 @@ $router->define([
     'posts' => 'PostsController',
     'addPost' => 'PostsController@addPost',
     'post' => 'PostsController@showPost',
-    'editPost' => 'PostsController@editPost',
     'updatePost' => 'PostsController@updatePost',
     'deletePost' => 'PostsController@deletePost',
 
     'register' => 'UsersController@register',
     'login' => 'UsersController@login',
     'logout' => 'UsersController@logout',
-    /*
-'index' => 'IndexController',
-'about' => 'AboutController',
-'tasks' => 'TaskController',
-'task' => 'TaskController@show',
-'add_task' => 'TaskController@showAddView',
-'parse_add_form' => 'TaskController@parseInput'
- */
+
 ]);
